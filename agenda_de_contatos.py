402ceb7c5e0dbea4795255ee2ef6f6fc340928b8
uriel_rodrigues <uriel.bezerra.rodrigues@gmail.com>
2024-06-26 15:07:11 -0300

    agenda_de_contatos menu principal e retorno ao menu

--- agenda_de_contatos.py
+++ agenda_de_contatos.py
@@ -7,41 +7,52 @@
 dados=[]
 registros = []
 
+def menu_principal():
+    print("--------------------------------------")
+    print("|      ->AGENDA DE CONTATOS <-       |")
+    print("--------------------------------------")
+    print("|      [1] -> Mostrar contatos       |")
+    print("|      [2] -> Adicionar contatos     |")
+    print("|      [3] -> Deletar contato        |")
+    print("--------------------------------------\n")
+    resposta = int(input("Digite o numero correspondente a sua escolha: "))
+
+    if resposta == 1:
+        mostrar_contatos()
+    elif resposta == 2:
+        adiconar_contatos()
+    elif resposta == 3:
+        deletar_contatos()
+
+
 def mostrar_contatos():
     for i in range(0, len(registros)):
-        print("{} -- {}".format(registros[i][0],registros[i][1]))
+        print("{} -- {}".format(registros[i][0], registros[i][1]))
+    menu_principal()
 
-def adiconar_dados():
-    for i in range(0,4):
+def adiconar_contatos():
+    quantidade = int(input("numero de contatos a adicionar: "))
+    for i in range(0, quantidade):
         dados.append(str(input("nome do contato: ")))
         dados.append(str(input("numero do contato: ")))
 
         registros.append(dados[:])
         dados.clear()
+    print("agenda de contatos atualizada ")
+    menu_principal()
 
-def deletar_dados():
+def deletar_contatos():
     nome = str(input("nome do contato para deletar: "))
 
     i = 0
-    parada = False
-    while parada == False:
-        while i < len(registros):
-            for i in range(0, len(registros)):
-                for p in range (0, 1):
-                    if nome in registros[i][0]:
-                        registros.pop(i)
-                        print("contato deletado")
-                        parada = True
+    while i < len(registros):
+        if nome in registros[i][0]:
+            registros.pop(i)
+            print("contato deletado")
 
-            i = i + 1
-    return registros
+        i = i + 1
+    print("{} foi retirado da sua lsta de contatos".format(nome))
+    menu_principal()
 
-
-adiconar_dados()
-mostrar_contatos()
-
-deletar_dados()
-
-print(registros)
-
+menu_principal()
 
